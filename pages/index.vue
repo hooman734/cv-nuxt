76b2567dbf09ccf08ce716f7be6064a4ff6d85a9
hooman734 <hesamyan@outlook.com>
2021-04-25 02:59:09 +0400

    first and working commit

---
+++ pages/index.vue
@@ -0,0 +1,341 @@
+<template>
+  <div class="mx-4 px-5 md:mx-9 md:px-9 my-1">
+    <div class="place-self-center">
+      <!-- introduction -->
+      <div class="pd-8 py-6 text-gray-900 hyphens-manual">
+        <h1 class="py-2 font-black text-5xl text-green-900 leading-none">Hooman Hesamyan</h1>
+        <p class="text-sm text-green-900 leading-tight text-2xl font-semibold border-b-8 pb-2">Seeking a Full-Stack Software Development
+          position</p>
+        <!-- end introduction -->
+        <!-- links -->
+        <div class="grid sm:grid-cols-2 my-2 py-1">
+          <p class="sm:col-span-1 text-gray-600 text-sm text-medium">Komitas Street, Yerevan, Armenia</p>
+          <div class="flex sm:col-span-1 justify-start sm:justify-end my-2 sm:my-0">
+            <table>
+              <tbody>
+              <tr class="leading-tight">
+                <th class="text-left font-bold">Email:</th>
+                <th class="text-sm text-right px-1 text-gray-600">hesamyan@gmail.com</th>
+              </tr>
+              <tr class="leading-tight">
+                <th class="text-left font-bold">Website:</th>
+                <th class="text-sm text-right px-1 text-gray-600">hesamian.com</th>
+              </tr>
+              <tr class="leading-tight">
+                <th class="text-left font-bold">Linkedin:</th>
+                <th class="text-sm text-right px-1 text-gray-600">linkedin.com/in/hesamyan</th>
+              </tr>
+              <tr class="leading-tight">
+                <th class="text-left font-bold">GitHub:​</th>
+                <th class="text-sm text-right px-1 text-gray-600">github.com/hooman734</th>
+              </tr>
+              <tr class="leading-tight">
+                <th class="text-left font-bold">Phone:</th>
+                <th class="text-sm text-right px-3 text-gray-600">+374-77-281-774</th>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <!-- end links -->
+        <!-- education -->
+        <div class="grid lg:grid-cols-2 my-2 lg:my-0">
+          <div class="lg:col-span-1 my-2 lg:my-0">
+            <div class="mb-3">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="graduation-cap"/>
+                Education
+              </h3>
+            </div>
+            <!-- AUA -->
+            <div class="flex flex-wrap flex-row mb-2">
+              <span class="border-r-4 italic pr-3">2017 - 2020</span>
+              <div class="m-0 sm:ml-2">
+                <ul class="list-outside mx-5">
+                  <li class="py-1">
+                    <p><span
+                      class="font-black">American University of Armenia (AUA), Yerevan, Armenia</span>
+                    </p>
+                    <p class="font-semibold">Master of Science in Computer Science</p>
+                    <ul class="list-outside list-disc mx-5">
+                      <li>Data Structures</li>
+                      <li>Algorithms (Intermediate and Advanced)</li>
+                      <li>Object-Oriented Programming</li>
+                      <li>Theory of Computing</li>
+                      <li>Network Programming</li>
+                      <li>Computer-Aided Manufacturing (CAM)</li>
+                      <li>Cryptography</li>
+                      <li>Image Processing</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Azad -->
+            <div class="flex flex-wrap flex-row mb-2">
+              <span class="border-r-4 italic pr-3">2011 - 2014</span>
+              <div class="m-0 sm:ml-2">
+                <ul class="list-outside mx-5 text-gray-700 font-semibold">
+                  <li class="py-1">
+                    <p><span class="font-black">Azad University of Gorgan, Gorgan, Iran</span></p>
+                    <p>Bachelor of Science in Civil Engineering</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Babol -->
+            <div class="flex flex-wrap flex-row mb-2">
+              <span class="border-r-4 italic pr-3">1999 - 2004</span>
+              <div class="m-0 sm:ml-2">
+                <ul class="list-outside mx-5 text-gray-700 font-semibold">
+                  <li class="py-1">
+                    <p><span class="font-black">Babol Polytechnic, Babol, Iran</span></p>
+                    <p>Bachelor of Science in Mechanical Engineering</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- end education -->
+          <!-- technical skills -->
+          <div class="lg:col-span-1 my-4 lg:my-2">
+            <div class="mb-3">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="desktop"/>
+                Technical Skills
+              </h3>
+            </div>
+            <div class="m-0 md:ml-10">
+              <ul class="list-outside mx-5 text-gray-700">
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Languages
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="terminal"/>  Java, JavaScript, SQL, Python, C/C++, C#, Golang, Lua</span>
+                </li>
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Front-End
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="laptop-code"/>  HTML, CSS, jQuery, tailwind, bootstrap, Semantic-UI, Angular.js, Angular, React, Vue.js</span>
+                </li>
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Back-End
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="server"/>  Node.js, Express.js, .NET Core, Koa.js, Flask, Django, Spring Boot</span>
+                </li>
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Libraries
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="layer-group"/> Tkinter, Sequelize.js, Entity Framework.Core</span>
+                </li>
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Database / Storage
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="database"/>  SQLite, ​MySQL, Postgres, Oracle SQL, MongoDB, S3</span>
+                </li>
+                <li class="py-1 pb-2">
+                  <div class="font-black">
+                    Application Software
+                  </div>
+                  <span class="text-gray-800 font-semibold align-text-top ml-4"><font-awesome-icon icon="window-restore"/>  Git, ash, Linux, Heroku, Docker, docker-compose, localstack</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- end technical skills -->
+          <!-- experience -->
+          <div class="lg:col-span-1 my-4 lg:my-2">
+            <div class="mb-3">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="briefcase"/>
+                Experience
+              </h3>
+            </div>
+              <div class="flex flex-wrap flex-row">
+                <span class="border-r-4 italic pr-5">2020 - 2021</span>
+                <div class="m-0 sm:ml-2">
+                  <ul class="list-outside mx-5">
+                    <li class="py-1">
+                      <p class="font-bold">Graduate School Internship Project</p>
+                      <ul class="list-disc list-inside mx-5 text-sm font-semibold">
+                        <li class="py=1">Used Python, Tkinter to create a download-manager with chunking support and GUI S3
+                          browser
+                        </li>
+                        <li class="py=1">Used Node.js, Socket.IO, and React/Redux to create an online chat room with channel
+                          support
+                        </li>
+                        <li class="py=1">Used .NET Core to create a public traffic transportation monitoring with route
+                          suggestions
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+          </div>
+          <!-- end experience -->
+          <!-- honors and awards -->
+          <div class="lg:col-span-1 my-4 lg:my-2">
+            <div class="mb-3">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="award"/>
+                Honors and Awards
+              </h3>
+            </div>
+            <div class="flex flex-wrap flex-row">
+              <span class="border-r-4 italic pr-5">2019 - 2020</span>
+              <div class="m-0 sm:ml-2">
+                <ul class="list-outside mx-5">
+                  <li class="py-1 font-semibold">
+                    <p>Chancellor Award and Aurora scholarship from American University of Armenia</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            </div>
+          <!-- end honors and awards -->
+          <!-- general skills -->
+          <div class="lg:col-span-1 my-4 lg:my-2">
+            <div class="mb-3">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="puzzle-piece"/>
+                General Skills
+              </h3>
+            </div>
+            <div class="m-0 md:ml-10">
+              <ul class="list-outside mx-5">
+                <li class="py-1">
+                  <p class="font-bold">IELTS General: <span
+                    class="text-gray-800 font-semibold align-text-top">6.5</span></p>
+                </li>
+                <li class="py-1">
+                  <p class="font-bold">TOEFL Academic: <span class="text-gray-800 font-semibold align-text-top">86</span>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- end general skills -->
+          <!-- blog -->
+          <div class="lg:col-span-1 my-4 lg:my-2">
+            <div class="mb-">
+              <h3 class="font-black text-3xl text-green-900">
+                <font-awesome-icon icon="pen-nib"/>
+                Blog
+              </h3>
+            </div>
+              <ul class="list-outside mx-5">
+                <li class="py-1">
+                  <span class="border-r-4 italic pr-5">Mar 3 - 2021</span>
+                  <div class="m-0 ml-4">
+                    <p class="font-bold"><a target="_blank" href="https://hesamyan.medium.com/experience-with-f-618118e99408">Experience With F#</a></p>
+                  </div>
+                </li>
+                <li class="py-1">
+                  <span class="border-r-4 italic pr-5">Jan 30 - 2021</span>
+                  <div class="m-0 ml-4">
+                    <p class="font-bold"><a target="_blank" href="https://hesamyan.medium.com/setting-up-dockerfile-github-workflow-86cafd82cf97">Setting up Dockerfile & GitHub Workflow</a></p>
+                  </div>
+                </li>
+                <li class="py-1">
+                  <span class="border-r-4 italic pr-5">Dec 31 - 2020</span>
+                  <div class="m-0 ml-4">
+                    <p class="font-bold"><a target="_blank" href="https://hesamyan.medium.com/implementing-four-types-of-linked-lists-d812d5c39812">Implementing four types of linked-lists</a></p>
+                  </div>
+                </li>
+                <li class="py-1">
+                  <span class="border-r-4 italic pr-5">Dec 24 - 2020</span>
+                  <div class="m-0 ml-4">
+                    <p class="font-bold"><a target="_blank" href="https://hesamyan.medium.com/switching-from-networkmanager-to-systemd-networkd-dcbda0b15056">Switching from NetworkManager to systemd-networkd</a></p>
+                  </div>
+                </li>
+                <li class="py-1">
+                  <span class="border-r-4 italic pr-5">Dec 9 - 2020</span>
+                  <div class="m-0 ml-4">
+                    <p class="font-bold"><a target="_blank" href="https://hesamyan.medium.com/deploying-a-react-web-application-thats-created-by-create-react-app-2234ddf8eafa">Deploying a React Web Application that's created by create-react-app</a></p>
+                  </div>
+                </li>
+              </ul>
+          </div>
+          <!-- end blog -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+  faBriefcase,
+  faGraduationCap,
+  faPuzzlePiece,
+  faDesktop,
+  faAward,
+  faLink,
+  faLaptopCode,
+  faTerminal,
+  faServer,
+  faDatabase,
+  faLayerGroup,
+  faWindowRestore,
+  faPenNib
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(faBriefcase, faGraduationCap, faPuzzlePiece, faDesktop, faAward, faLink, faLaptopCode, faTerminal, faServer, faDatabase, faLayerGroup, faWindowRestore, faPenNib)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+export default Vue.extend({})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
